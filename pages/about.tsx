2bf52215d58a99b97e69918a4390a03945b6be5d
shinjihoon <jihoon.shin@teoul.com>
2022-09-07 19:42:20 +0900

    next.js 튜토리얼 진행 중

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+import Layout from "./components/MyLayout";
+
+export default function About() {
+    return <div>
+        <Layout>
+            <p>About!!!!!</p>
+        </Layout>
+    </div>
+}
